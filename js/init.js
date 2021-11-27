03840b50b4f403a84fe8e7470b446d32d4bd1959
maru1pir <maru1pir@gmail.com>
2021-11-26 22:49:18 -0300

    pruebagooglesignout3

--- js/init.js
+++ js/init.js
@@ -60,8 +60,15 @@ function signOut(){
   auth2.signOut().then(function(){
     console.log('User signed out.');
   });
+  auth2.disconnect();
 
 }
+
+/*gapi.auth2.getAuthInstance().disconnect();
+var auth2 = gapi.auth2.getAuthInstance();
+auth2.signOut().then(function () {
+});
+auth2.disconnect();*/
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
