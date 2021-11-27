70f743508b6f72af241be91405e2dd7239951862
maru1pir <maru1pir@gmail.com>
2021-11-26 23:13:59 -0300

    pruebagooglelogout5

--- js/init.js
+++ js/init.js
@@ -56,16 +56,22 @@ function showData(){
 };
 
 function signOut(){
-  var auth2 = gapi.auth2.getAuthInstance();
+  if (window.gapi) {
+    const auth2 = window.gapi.auth2.getAuthInstance()
+    if (auth2 != null) {
+      auth2.signOut().then(auth2.disconnect().then(this.props.onLogoutSuccess))
+    }
+ }
+
+}
+
+/*var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function(){
     console.log('User signed out.');
     auth2.disconnect();
   });
   auth2.disconnect();
-
-}
-
-/*signOut() {
+  signOut() {
   if (window.gapi) {
      const auth2 = window.gapi.auth2.getAuthInstance()
      if (auth2 != null) {
